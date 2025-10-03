cb01b638e0821d53abcc7c7f2a57267ac6f30f78
TheCrazy8 <bennettbashford@gmail.com>
2025-10-03 12:54:15 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+questions = []
+
+def addq():
+    q = input("What question would you like to add? ")
+    questions.append(q)
+    qadloop()
+    
+def qadloop():
+    if input("Add another? ") == "y":
+        addq()
+    else:
+        pass
+
+def askq():
+    pass
+
+def main():
+    addq()
+    
+if __name__ == '__main__':
+    main()
